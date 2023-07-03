f4e6ad90145f08510eef24ea750b499c58ed51d3
NowScott <nowscott@qq.com>
2023-07-03 21:14:08 +0800

    Update scrape.js

--- src/scrape.js
+++ src/scrape.js
@@ -1,21 +1,20 @@
 const axios = require('axios');
 const fs = require('fs');
-
 // 发起 HTTP 请求并获取数据
 axios.get('https://web.nowscott.top/.netlify/functions/notion')
-  .then(response => {
-    // 处理并解析返回的数据
-    const data = response.data;
-
-    // 将数据保存为 JSON 文件
-    const jsonData = JSON.stringify(data);
-    fs.writeFile('data.json', jsonData, (err) => {
-      if (err) {
-        console.error('Error writing JSON file:', err);
-      } else {
-        console.log('Data saved as JSON file');
-      }
+    .then(response => {
+        // 处理并解析返回的数据
+        const data = response.data;
+        // 格式化 JSON 数据
+        const formattedJson = JSON.stringify(data, null, 4);
+        // 将数据保存为 JSON 文件
+        fs.writeFile('data.json', formattedJson, (err) => {
+            if (err) {
+                console.error('Error writing JSON file:', err);
+            } else {
+                console.log('Data saved as JSON file');
+            }
+        });
+    }).catch(error => {
+        console.error('Error fetching data:', error);
     });
-  }).catch(error => {
-    console.error('Error fetching data:', error);
-  });
