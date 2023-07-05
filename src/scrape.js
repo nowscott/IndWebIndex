cd8f89bb4dd69e4846bbaa0b02dd8e7969b277b5
NowScott <nowscott@qq.com>
2023-07-05 12:33:42 +0800

    Update scrape.js

--- src/scrape.js
+++ src/scrape.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 // 发起 HTTP 请求并获取数据
-axios.get('https://web.nowscott.top/.netlify/functions/notion')
+axios.get('https://nowpage.netlify.app/.netlify/functions/notion')
   .then(response => {
     // 处理并解析返回的数据
     const data = response.data;
