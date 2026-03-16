fdc2a596e948d1fbaabf5897dee0a9201bb917dc
NowScott <nowscott@qq.com>
2026-03-16 21:05:44 +0800

    feat(字体): 添加芫荽和汇文明朝体两种新字体选项
    - 在字体选择菜单中新增芫荽 (Iansui) 和汇文明朝体 (Huiwen-Mincho) 选项
    - 将外部字体样式表从 CSS @import 迁移至 HTML <link> 标签以优化加载
    - 为字体菜单项添加预览样式类，移除内联样式以提升可维护性

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,16 @@ class MyDocument extends Document {
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="description" content="探索IndWebIndex——您的个性化中文网站索引！我们提供一个简单且高效的方法，让您可以快速访问和管理您常用的网站。无论是学习资源、购物平台还是娱乐网站，您都可以一目了然。" />
+          
+          {/* External Fonts */}
+          <link rel="stylesheet" href="https://f.0211120.xyz/font/得意黑/result.css" crossorigin="anonymous"/>
+          <link rel="stylesheet" href="https://f.0211120.xyz/font/京華老宋体/result.css" crossorigin="anonymous"/>
+          <link rel="stylesheet" href="https://f.0211120.xyz/font/霞鹜文楷/result.css" crossorigin="anonymous"/>
+          <link rel="stylesheet" href="https://f.0211120.xyz/font/霞鹜漫黑/result.css" crossorigin="anonymous"/>
+          <link rel="stylesheet" href="https://f.0211120.xyz/font/芫荽/result.css" crossorigin="anonymous"/>
+          <link rel="stylesheet" href="https://f.0211120.xyz/font/汇文明朝体/result.css" crossorigin="anonymous"/>
+          <link rel="stylesheet" href="https://chinese-fonts-cdn.deno.dev/chinesefonts3/packages/mzxst/dist/MZPXorig/result.css" crossorigin="anonymous"/>
+          
           <script
             dangerouslySetInnerHTML={{
               __html: `
